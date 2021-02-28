c2f216d8b4d79aa1776974b5558101d0e20fb194
tamosimona2000 <58388645+tamosimona2000@users.noreply.github.com>
2021-02-28 17:57:09 +0400

    Created Database

---
+++ BookStore.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base("BookStoreConnection")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
---
+++ BookStore.DataAccess.SQL/Migrations/202102281349595_Initial.cs
@@ -0,0 +1,43 @@
+namespace BookStore.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductCategories",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 128),
+                        CategoryName = c.String(),
+                        CreationTime = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 100),
+                        Description = c.String(nullable: false, maxLength: 500),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Category = c.String(nullable: false),
+                        Image = c.String(),
+                        Author = c.String(nullable: false, maxLength: 100),
+                        CreationTime = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductCategories");
+        }
+    }
+}
