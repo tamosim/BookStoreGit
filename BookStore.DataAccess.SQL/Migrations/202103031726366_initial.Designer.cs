91b2f5d582f6c4ac545f6a6ab5d8143fa20ddaea
tamosimona2000 <58388645+tamosimona2000@users.noreply.github.com>
2021-03-03 21:50:38 +0400

    entyty framework

--- BookStore.DataAccess.SQL/Migrations/202103031726366_initial.Designer.cs
+++ BookStore.DataAccess.SQL/Migrations/202103031726366_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace BookStore.DataAccess.SQL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class Initial : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "202102281349595_Initial"; }
+            get { return "202103031726366_initial"; }
         }
         
         string IMigrationMetadata.Source
