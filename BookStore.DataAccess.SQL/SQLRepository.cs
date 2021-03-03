0325adfe880d610ece3ffdd84d705779176cf8d1
tamosimona2000 <58388645+tamosimona2000@users.noreply.github.com>
2021-03-03 22:15:51 +0400

    Implemented sql repository

---
+++ BookStore.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,57 @@
+﻿using BookStore.Core.Contracts;
+using BookStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Delete(string ID)
+        {
+            var t = Find(ID);
+            if (context.Entry(t).State == EntityState.Detached)
+            {
+                dbSet.Attach(t);
+            }
+            dbSet.Remove(t);
+        }
+
+        public T Find(string ID)
+        {
+            return dbSet.Find(ID);
+        }
+
+        public void Insert(T t)
+        {
+            dbSet.Add(t);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            dbSet.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
