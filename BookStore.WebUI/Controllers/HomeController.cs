a2459f11a7f5455173b48bbadcc0e7bde9b4fdf9
tamosimona2000 <58388645+tamosimona2000@users.noreply.github.com>
2021-03-04 21:42:44 +0400

    added product list to main page

--- BookStore.WebUI/Controllers/HomeController.cs
+++ BookStore.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookStore.Core.Contracts;
+using BookStore.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,30 @@ namespace BookStore.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> productlist = context.Collection().ToList();
+            return View(productlist);
         }
-
+        public ActionResult Details(string id)
+        {
+            Product product = context.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
